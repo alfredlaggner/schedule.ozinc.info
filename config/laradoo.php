704b67572c183207568b9917f6913d010ea75a50
Alfred Laggner <alfred.laggner@gmail.com>
2020-09-01 13:47:40 -0700

    for new version

--- config/laradoo.php
+++ config/laradoo.php
@@ -7,7 +7,7 @@
 		'host'       => 'https://ozinc-production.odoo.com/',
 		'db'         => 'ozdistribution-odooshv11-production-70102',
 		'username'   => 'alfred.laggner@gmail.com',
-		'password'   => 'jahai999',
+		'password'   => 'jahai555',
 /*
 		'host'       => 'https://ozinc-production-elf-test-1033330.dev.odoo.com/',
 		'db'         => 'ozinc-production-elf-test-1033330',
